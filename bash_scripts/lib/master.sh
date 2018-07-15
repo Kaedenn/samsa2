7a4c1f3bceda3e2693dcfb931237ee2d040741f4
Peter Schultz <kaedenn@gmail.com>
2018-07-14 17:56:05 -0800

    Only trim merged.assembled files

--- bash_scripts/lib/master.sh
+++ bash_scripts/lib/master.sh
@@ -44,12 +44,12 @@ do_pear() {
 
 # do_trimmomatic <input-path> <output-path>
 do_trimmomatic() {
-  # <f>.merged.*.fastq -> <f>.cleaned.fastq
+  # <f>.merged.*.fastq -> <f>.cleaned.
   local ipath="$1"
   local opath="$2"
   do_mkdir "$opath"
   for mf in $ipath/*.merged.*; do
-    ofile="$(repath "$mf" "$opath" 's|\.merged|.cleaned.fastq|')"
+    ofile="$(repath "$mf" "$opath" 's|\.merged\.assembled*|.cleaned.fastq|')"
     do_java -jar $TRIMMOMATIC SE -phred33 $mf $ofile SLIDINGWINDOW:4:15 MINLEN:99
     if [[ $? -ne 0 ]]; then
       logfail trimmomatic "$mf" "$ofile"
