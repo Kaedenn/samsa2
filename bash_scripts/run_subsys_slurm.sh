ca63240c1a05a924857bfdaf11bd7c1a695bb3b0
Peter Schultz <kaedenn@gmail.com>
2018-09-15 14:26:25 -0800

    change prefix to a string instead of a function

--- bash_scripts/run_subsys_slurm.sh
+++ bash_scripts/run_subsys_slurm.sh
@@ -8,7 +8,7 @@ run_program()
   srun -p t1small --pty --exclusive "$@"
 }
 
-export PREFIX=run_program
+export PREFIX="srun -p t1small --pty --exclusive"
 
 export DRY_RUN=1
 do_diamond_subsys "$STEP_3" "$STEP_4"
